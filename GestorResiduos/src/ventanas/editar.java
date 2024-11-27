d8f07c248fec868f9f72f74a2474ad57194c8006
jdavidespinoza05 <jdavidespinoza05@gmail.com>
2024-11-27 04:21:04 -0600

    Update editar.java

--- GestorResiduos/src/ventanas/editar.java
+++ GestorResiduos/src/ventanas/editar.java
@@ -31,11 +31,9 @@ public class editar extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
